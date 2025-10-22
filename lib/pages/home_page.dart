52751e9f4f732d9924937e8f866392f518fbf0bc
Mathias Martins <mathias.martins@isolutions.com.br>
2025-10-21 21:26:32 -0300

    Ajustes padding homepage e footer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,59 +13,72 @@ class HomePage extends StatelessWidget {
         child: Header(),
       ),
       body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 40),
         children: [
           const SizedBox(height: 20),
-          const Text(
-            'SERVIÇO DE CALHAS COM MÃO DE OBRA QUALIFICADA',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 42,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 1.2,
+
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'SERVIÇO DE CALHAS COM MÃO DE OBRA QUALIFICADA',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 42,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
             ),
           ),
+
           const SizedBox(height: 15),
-          const Text(
-            'Fale com quem entende do assunto',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              height: 1.5,
+
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Fale com quem entende do assunto',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, height: 1.5),
             ),
           ),
+
           const SizedBox(height: 30),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/contact');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFFDC193A), // cor de fundo em hexadecimal
-                foregroundColor: Colors.white,              // cor do texto
-                padding: const EdgeInsets.symmetric(vertical: 25),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: SizedBox(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/contact');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFDC193A),
+                  // cor de fundo em hexadecimal
+                  foregroundColor: Colors.white,
+                  // cor do texto
+                  padding: const EdgeInsets.symmetric(vertical: 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'ENTRE EM CONTATO CONOSCO',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.1,
+                  ),
                 ),
               ),
-              child: const Text(
-                'ENTRE EM CONTATO CONOSCO',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 1.1,
-                ),
-              ),
-            )
+            ),
           ),
           const SizedBox(height: 30),
 
-          // 1 - Imagem full width
-          Image.asset(
-            'assets/images/fotosCalhas/calha5.jpg',
-            width: double.infinity,
-            fit: BoxFit.cover,
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Image.asset(
+              'assets/images/fotosCalhas/calha5.jpg',
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
           ),
 
           const SizedBox(height: 30),
@@ -106,9 +119,9 @@ class HomePage extends StatelessWidget {
                       const Text(
                         'Nos destacamos no comércio e distribuição de perfis e chapas de alumínio, conquistando a confiança de nossos clientes com produtos de alta qualidade. Em 2010, expandimos nossas operações para o segmento de fabricação e instalação de calhas, rufos, pingadeiras e condutores, além de coifas, dutos e chaminés sob medida, atendendo a uma demanda crescente por soluções personalizadas e eficientes. Ao longo de nossa trajetória, temos investido continuamente no aperfeiçoamento de nossas técnicas de montagem e instalação, sempre com o objetivo de garantir a segurança e a durabilidade de nossos produtos.',
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16,
-                            height: 1.5
+                          color: Colors.white,
+                          fontSize: 16,
+                          height: 1.5,
                         ),
                       ),
 
@@ -154,7 +167,8 @@ class HomePage extends StatelessWidget {
                 const SizedBox(height: 40),
 
                 Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround, // centraliza e distribui igualmente
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  // centraliza e distribui igualmente
                   children: [
                     _iconAboveText(Icons.bar_chart, 'Empresa especializada'),
 
@@ -219,7 +233,6 @@ class HomePage extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-
               const Text(
                 'Qualidade Garantida',
                 style: TextStyle(
@@ -231,41 +244,54 @@ class HomePage extends StatelessWidget {
 
               const SizedBox(height: 20),
 
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16), // ajusta o valor conforme quiser
-                child: Image.asset(
-                  'assets/images/fotosCalhas/calha15.jpg',
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.asset(
+                    'assets/images/fotosCalhas/calha15.jpg',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
 
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16), // ajusta o valor conforme quiser
-                child: Image.asset(
-                  'assets/images/fotosCalhas/calha16.jpg',
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  // ajusta o valor conforme quiser
+                  child: Image.asset(
+                    'assets/images/fotosCalhas/calha16.jpg',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
 
-              ClipRRect(
-                borderRadius: BorderRadius.circular(16), // ajusta o valor conforme quiser
-                child: Image.asset(
-                  'assets/images/fotosCalhas/calha17.jpg',
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  // ajusta o valor conforme quiser
+                  child: Image.asset(
+                    'assets/images/fotosCalhas/calha17.jpg',
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
-
             ],
           ),
 
-          // Footer
-          const Footer(),
+          Container(
+            width: double.infinity,
+            color: Colors.black, // fundo preto
+            child: const Footer(),
+          ),
         ],
       ),
     );
@@ -275,11 +301,7 @@ class HomePage extends StatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Icon(
-          icon,
-          color: const Color(0xFFDC193A),
-          size: 60,
-        ),
+        Icon(icon, color: const Color(0xFFDC193A), size: 60),
         const SizedBox(height: 12),
         Text(
           label,
--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -8,147 +8,158 @@ class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black, // fundo preto
-      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 40),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text(
-            'Sobre nós',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+      color: Colors.black,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: double.infinity,
+          child: Container(
+            color: Colors.black,
+            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 40),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  'Sobre nós',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 12),
+
+                const Text(
+                  'Desde 1997 nos destacamos no comércio e distribuição de perfis e chapas de alumínio, conquistando a confiança de nossos clientes com produtos de alta qualidade.',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/contact');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFDC193A),
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 30,
+                      vertical: 17,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    'Saiba Mais',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(height: 40),
+
+                const Text(
+                  'Endereço',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 12),
+
+                const Text(
+                  'Rua dos Suíços, 4466 - Vila Nova, Joinville - SC. 89237-720',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+
+                const Text(
+                  'Sac/Atendimento',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 12),
+
+                const Text(
+                  '(47) 3417-6465',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+
+                const Text(
+                  '(47) 3417-6464',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    height: 1.5,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 40),
+
+                const Text(
+                  'Conheça nossas Redes',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 12),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _socialButton(
+                      icon: FontAwesomeIcons.facebook,
+                      url:
+                          'https://www.facebook.com/share/9vLiaLtLfbVQ22BC/?mibextid=LQQJ4d',
+                    ),
+                    const SizedBox(width: 16),
+
+                    _socialButton(
+                      icon: FontAwesomeIcons.instagram,
+                      url:
+                          'https://www.instagram.com/_casadascalhas_?igsh=MWl3dDkzNGl6ZmJ5eQ%3D%3D',
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 30),
+
+                const Divider(color: Colors.white24, thickness: 0.5),
+                const SizedBox(height: 12),
+                const Text(
+                  '© 2025 Calhas Joinville. Active. Todos os direitos reservados.',
+                  style: TextStyle(fontSize: 12, color: Colors.white38),
+                  textAlign: TextAlign.center,
+                ),
+              ],
             ),
-            textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 12),
-
-          const Text(
-            'Desde 1997 nos destacamos no comércio e distribuição de perfis e chapas de alumínio, conquistando a confiança de nossos clientes com produtos de alta qualidade.',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 20),
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/contact');
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFFDC193A),
-              foregroundColor: Colors.white,
-              padding:
-              const EdgeInsets.symmetric(horizontal: 30, vertical: 17),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            child: const Text(
-              'Saiba Mais',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 40),
-
-          const Text(
-            'Endereço',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 12),
-
-          const Text(
-            'Rua dos Suíços, 4466 - Vila Nova, Joinville - SC. 89237-720',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 40),
-
-          const Text(
-            'Sac/Atendimento',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 12),
-
-          const Text(
-            '(47) 3417-6465',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-
-          const Text(
-            '(47) 3417-6464',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 16,
-              height: 1.5,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 40),
-
-          const Text(
-            'Conheça nossas Redes',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 12),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _socialButton(
-                icon: FontAwesomeIcons.facebook,
-                url: 'https://www.facebook.com/share/9vLiaLtLfbVQ22BC/?mibextid=LQQJ4d',
-              ),
-              const SizedBox(width: 16),
-
-              _socialButton(
-                icon: FontAwesomeIcons.instagram,
-                url: 'https://www.instagram.com/_casadascalhas_?igsh=MWl3dDkzNGl6ZmJ5eQ%3D%3D',
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 30),
-
-          const Divider(color: Colors.white24, thickness: 0.5),
-          const SizedBox(height: 12),
-          const Text(
-            '© 2025 Calhas Joinville. Active. Todos os direitos reservados.',
-            style: TextStyle(fontSize: 12, color: Colors.white38),
-            textAlign: TextAlign.center,
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -156,9 +167,6 @@ class Footer extends StatelessWidget {
   Widget _socialButton({required IconData icon, required String url}) {
     return GestureDetector(
       onTap: () async {
-        // abre o link no navegador
-        // adicione o package url_launcher no pubspec.yaml
-        // e importe: import 'package:url_launcher/url_launcher.dart';
         final Uri uri = Uri.parse(url);
         if (await canLaunchUrl(uri)) {
           await launchUrl(uri, mode: LaunchMode.externalApplication);
@@ -172,13 +180,7 @@ class Footer extends StatelessWidget {
           color: const Color(0xFF222222),
           shape: BoxShape.circle,
         ),
-        child: Center(
-          child: Icon(
-            icon,
-            color: Colors.white,
-            size: 17,
-          ),
-        ),
+        child: Center(child: Icon(icon, color: Colors.white, size: 17)),
       ),
     );
   }
